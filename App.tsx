4fe14753f96b84790446af94e716d12a962d724e
hamza-devO <hamza.hashmi@softnoxtechnologies.com>
2026-02-10 02:05:21 +0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-
+// import { setupPlayer } from 'react-native-track-player/lib/src/trackPlayer';
+import { setupPlayer } from './src/player/setupPlayer';
 const App = () => {
+  useEffect(() => {
+    setupPlayer();
+  }, []);
   return (
     <NavigationContainer>
       <AppNavigator />
